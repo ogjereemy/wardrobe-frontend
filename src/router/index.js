6be31e0ee44f2a2178e9a7088bc0517ee73fdb32
ogola jeremy <ogolajeremy601@gmail.com>
2025-02-28 12:56:42 +0300

    Removes bugs in routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/HomeView.vue'
-import Login from '../views/Login.vue'
-import Register from '../views/Register.vue'
-import ClothingItems from '../views/ClothingItems.vue'
+import Home from '@/views/HomeView.vue'
+import Login from '@/views/Login.vue'
+import Register from '@/views/Register.vue'
+import ClothingItems from '@/views/ClothingItems.vue'
 
 const routes = [
-  { path: '/', name: 'Home', component: Home },
-  { path: '/login', name: 'Login', component: Login },
-  { path: '/register', name: 'Register', component: Register },
-  { path: '/clothing-items', name: 'ClothingItems', component: ClothingItems },
+  { path: '/', component: Home },
+  { path: '/login', component: Login },
+  { path: '/register', component: Register },
+  { path: '/clothing-items', component: ClothingItems, meta: { requiresAuth: true } },
 ]
 
 const router = createRouter({
-  history: createWebHistory('/'),
+  history: createWebHistory(),
   routes,
+})
+
+router.beforeEach((to, from, next) => {
+  const isLoggedIn = !!localStorage.getItem('token')
+  if (to.matched.some((record) => record.meta.requiresAuth) && !isLoggedIn) {
+    next('/login')
+  } else {
+    next()
+  }
 })
 
 export default router
